fc8867278be08470c72d78bdcf85a0a1d5d9e649
zm18025 <zm18025@josai.ac.jp>
2019-11-12 03:26:11 +0000

    2019/11/12

---
+++ ex6-6.js
@@ -0,0 +1,13 @@
+var who = 'world';
+function sayhello66(){
+    alert('Hello,' + who + '!');
+}
+function taro66(){
+    who = '太郎';
+}
+function hanako66(){
+    who = '花子';
+}
+function gakita66(){
+    alert(who + 'が来た');
+}
