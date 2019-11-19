85f8d88aa6a4c6ae7d65d602ec904e450e908ecb
zm18025 <zm18025@josai.ac.jp>
2019-11-19 03:43:51 +0000

    2019/11/19

--- ex6-9.js
+++ ex6-9.js
@@ -1,1 +1,6 @@
-setTimeout('alert("3 seconds have passed");',3000);
+function txtchange1(){
+    document.getElementById('txt').innerHTML = 'ナンバワン！';
+}
+function imgchange1(){
+    document.getElementById('long').src = './image1/bulbon.jpg';
+}
