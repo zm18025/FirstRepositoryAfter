a8728270e60ec57707bef236c2d46af95db21f1b
zm18025 <zm18025@josai.ac.jp>
2019-12-24 03:05:23 +0000

    12/24

--- ex07-3.js
+++ ex07-3.js
@@ -9,7 +9,32 @@ function add(){
 function BMI(){
     var w = document.getElementById('weight').value;
     var h = document.getElementById('height').value;
-    twice = parseInt(h)*parseInt(h);
-    Body = parseInt(w)/parseInt(twice);
-    alert(Body);
-}
+    twice = parseFloat(h)*parseFloat(h);
+    Body = parseFloat(w)/parseFloat(twice);
+    alert(Body);}
+function result(){
+    if (Body < 16) {
+    alert("痩せすぎ");
+
+    } else if (Body <= 16.99) {
+    alert("痩せ");
+
+    } else if (Body <= 18.49) {
+    alert("痩せぎみ");
+
+    } else if (Body <= 24.99) {
+    alert("普通");
+
+     } else if (Body <= 29.99) {
+    alert("前肥満");
+
+    } else if (Body <= 34.99) {
+    alert(Body + "肥満（１）");
+
+    } else if (Body <= 39.99) {
+    alert("肥満（２）");
+
+    } else{
+    alert("肥満（３）");
+    }
+    }
